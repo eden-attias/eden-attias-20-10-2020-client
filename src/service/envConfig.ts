dfaa09f1affbcff3cf38f486cc15c5dd116e3712
eden-attias <73401983+eden-attias@users.noreply.github.com>
2020-10-26 10:36:31 +0200

    Update envConfig.ts

--- src/service/envConfig.ts
+++ src/service/envConfig.ts
@@ -3,14 +3,7 @@
  */
 
 export default class EnvConfig {
-    public static getAppEnv() {
-        return process.env.APP_ENV;
-    }
-
-    public static getNodeEnv() {
-        return process.env.NODE_ENV;
-    }
-
+ 
     public static getApiUrlFromServer() {
         return process.env.REACT_APP_APP_API_URL_SERVER;
     }
